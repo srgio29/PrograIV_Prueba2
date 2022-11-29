05540b4b96082eac5b8d6922e7bcb2ba6318b12c
srgio29 <sergio.barahona1994@gmail.com>
2022-11-28 19:27:59 -0600

    datasource para la prueba

---
+++ backend-recursoshumanos/src/datasources/conn.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'conn',
+  connector: 'mongodb',
+  url: 'mongodb+srv://sdbm29:<password>@cluster0.t8psbn2.mongodb.net/test',
+  host: 'cluster0.t8psbn2.mongodb.net',
+  port: 27018,
+  user: 'sdbm29',
+  password: 'S3v3rla29!',
+  database: 'recursoshumanos',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class ConnDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'conn';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.conn', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
